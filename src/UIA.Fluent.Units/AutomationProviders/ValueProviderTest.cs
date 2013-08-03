2ed9219a564014fa48e286c2f3a75d5b7cdaa60a
Levi Wilson <levi@leviwilson.com>
2013-08-03 16:49:03 -0400

    units around ValueProvider

---
+++ src/UIA.Fluent.Units/AutomationProviders/ValueProviderTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Automation;
+using System.Windows.Forms;
+using NUnit.Framework;
+using Should.Fluent;
+
+namespace UIA.Fluent.AutomationProviders
+{
+    [TestFixture]
+    public class ValueProviderTest
+    {
+
+        [Test]
+        public void ItHasTheCorrectPattern()
+        {
+            var valueProvider = GetValueProvider();
+
+            valueProvider.GetPatternProvider(ValuePatternIdentifiers.Pattern.Id)
+                .Should().Be.SameAs(valueProvider);
+        }
+
+        [Test]
+        public void ValuesCanBeRetrieved()
+        {
+            GetValueProvider(() => "Expected Value").Value
+                .Should().Equal("Expected Value");
+        }
+
+        [Test]
+        public void ValuesCanBeSet()
+        {
+            var expectedValue = string.Empty;
+
+            GetValueProvider(null, (x) => expectedValue = x)
+                .SetValue("The expected value to be set");
+
+            expectedValue.Should().Equal("The expected value to be set");
+        }
+
+        private ValueProvider GetValueProvider(Func<string> getter = null, Action<string> setter = null)
+        {
+            return new ValueProvider(new Control(), getter, setter);
+        }
+    }
+}
