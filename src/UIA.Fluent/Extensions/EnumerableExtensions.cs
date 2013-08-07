3dd3ebae7ab2a7422554d36a6a68e63a923c2f9d
Levi Wilson <levi@leviwilson.com>
2013-08-07 14:05:15 -0400

    units around EnumerableExtensions

--- src/UIA.Fluent/Extensions/EnumerableExtensions.cs
+++ src/UIA.Fluent/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace UIA.Fluent.Extensions
@@ -15,19 +14,7 @@ namespace UIA.Fluent.Extensions
         public static T After<T>(this IList<T> items, T what)
         {
             var indexOfWhat = items.IndexOf(what);
-            return items.ElementAtOrDefault(indexOfWhat + 1);
-        }
-
-        public static int IndexOf<T>(this IEnumerable<T> items, Func<T, bool> trueCondition)
-        {
-            var foundIndex = 0;
-            foreach (var item in items)
-            {
-                if (trueCondition(item)) return foundIndex;
-                ++foundIndex;
-            }
-
-            return -1;
+            return indexOfWhat == -1 ? default(T) : items.ElementAtOrDefault(indexOfWhat + 1);
         }
     }
 }
