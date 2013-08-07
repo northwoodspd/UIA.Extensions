012bf403f2d19965c10aa71f99f25779ea0aa842
Levi Wilson <levi@leviwilson.com>
2013-08-07 13:43:56 -0400

    move enumerable extensions into its own file

--- src/UIA.Fluent/Extensions/AutomationExtensions.cs
+++ src/UIA.Fluent/Extensions/AutomationExtensions.cs
@@ -1,39 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 using UIA.Fluent.AutomationProviders;
 using UIA.Fluent.AutomationProviders.Tables;
 
 namespace UIA.Fluent.Extensions
 {
-    public static class EnumerableExtensions
-    {
-        public static T Before<T>(this IList<T> items, T what)
-        {
-            var indexOfWhat = items.IndexOf(what);
-            return items.ElementAtOrDefault(indexOfWhat - 1);
-        }
-
-        public static T After<T>(this IList<T> items, T what)
-        {
-            var indexOfWhat = items.IndexOf(what);
-            return items.ElementAtOrDefault(indexOfWhat + 1);
-        }
-
-        public static int IndexOf<T>(this IEnumerable<T> items, Func<T, bool> trueCondition)
-        {
-            var foundIndex = 0;
-            foreach (var item in items)
-            {
-                if (trueCondition(item)) return foundIndex;
-                ++foundIndex;
-            }
-
-            return -1;
-        }
-    }
-
     public static class AutomationExtensions
     {
         public static AutomationConfigurer ExposeAutomation(this Control control)
---
+++ src/UIA.Fluent/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UIA.Fluent.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static T Before<T>(this IList<T> items, T what)
+        {
+            var indexOfWhat = items.IndexOf(what);
+            return items.ElementAtOrDefault(indexOfWhat - 1);
+        }
+
+        public static T After<T>(this IList<T> items, T what)
+        {
+            var indexOfWhat = items.IndexOf(what);
+            return items.ElementAtOrDefault(indexOfWhat + 1);
+        }
+
+        public static int IndexOf<T>(this IEnumerable<T> items, Func<T, bool> trueCondition)
+        {
+            var foundIndex = 0;
+            foreach (var item in items)
+            {
+                if (trueCondition(item)) return foundIndex;
+                ++foundIndex;
+            }
+
+            return -1;
+        }
+    }
+}
