c730ad6b734a3046686eafc23631bb87d0f31f14
Levi Wilson <levi@leviwilson.com>
2013-08-07 12:09:46 -0400

    header providers have the correct control type

---
+++ src/UIA.Fluent.Units/AutomationProviders/HeaderProviderTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Automation;
+using Moq;
+using NUnit.Framework;
+using Should.Fluent;
+
+namespace UIA.Fluent.AutomationProviders
+{
+    [TestFixture]
+    public class HeaderProviderTest
+    {
+        [Test]
+        public void ItHasTheCorrectControlType()
+        {
+            var table = new Mock<AutomationProvider>();
+            new HeaderProvider(table.Object).GetPropertyValue(AutomationElementIdentifiers.ControlTypeProperty.Id)
+                .Should().Equal(ControlType.Header.Id);
+        }
+    }
+}
--- src/UIA.Fluent/AutomationProviders/HeaderProvider.cs
+++ src/UIA.Fluent/AutomationProviders/HeaderProvider.cs
@@ -1,9 +1,15 @@
-﻿namespace UIA.Fluent.AutomationProviders
+﻿using System.Windows.Automation;
+
+namespace UIA.Fluent.AutomationProviders
 {
     public class HeaderProvider : ChildProvider
     {
         public HeaderProvider(AutomationProvider parent) : base(parent)
+        {}
+
+        protected override int ControlTypeId
         {
+            get { return ControlType.Header.Id; }
         }
     }
 }
