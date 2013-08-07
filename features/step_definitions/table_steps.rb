71c55224402eb1f1a9b3ac357e8df5fa8b208578
Levi Wilson <levi@leviwilson.com>
2013-08-07 00:12:18 -0400

    failing scenario for retrieving headers

--- features/step_definitions/table_steps.rb
+++ features/step_definitions/table_steps.rb
@@ -7,3 +7,7 @@ end
 Then(/^then the number of rows in the table should be "([^"]*)"$/) do |expected_rows|
   on(MainScreen).the_grid.count.should eq(expected_rows.to_i)
 end
+
+Then(/^the table headers should be "([^"]*)"$/) do |expected_headers|
+  on(MainScreen).the_grid_headers.should eq(expected_headers.split(', '))
+end
--- features/table.feature
+++ features/table.feature
@@ -3,3 +3,6 @@ Feature: Automating table controls
   Scenario: Getting the correct row count
     When we add "10" rows to the table
     Then then the number of rows in the table should be "10"
+
+  Scenario: Getting headers
+    Then the table headers should be "FirstName, LastName, Age"
