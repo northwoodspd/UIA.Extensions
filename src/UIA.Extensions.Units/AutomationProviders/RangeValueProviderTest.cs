cdb6fbd7e8e2387d69804d68f5de784ca2d29295
Levi Wilson <levi@leviwilson.com>
2013-08-09 13:45:29 -0400

    introduce the RangeValueProvider

---
+++ src/UIA.Extensions.Units/AutomationProviders/RangeValueProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Automation;
+using System.Windows.Forms;
+using NUnit.Framework;
+using Should.Fluent;
+
+namespace UIA.Extensions.AutomationProviders
+{
+    [TestFixture]
+    class RangeValueProviderTest
+    {
+        private RangeValueProvider _provider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _provider = new RangeValueProvider(new Control());
+        }
+
+        [Test]
+        public void ItHasTheCorrectPattern()
+        {
+            _provider.GetPatternProvider(RangeValuePatternIdentifiers.Pattern.Id)
+                .Should().Be.SameAs(_provider);
+        }
+    }
+}
---
+++ src/UIA.Extensions/AutomationProviders/RangeValueProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace UIA.Extensions.AutomationProviders
+{
+    public class RangeValueProvider : ControlProvider
+    {
+        public RangeValueProvider(Control control) : base(control)
+        {
+        }
+
+        protected override List<int> SupportedPatterns
+        {
+            get { return new List<int> { RangeValuePatternIdentifiers.Pattern.Id }; }
+        }
+    }
+}
