4073ce6a31e2ac7285e962a8c9d743f09ab9fea0
Jacob Ewald <jacob.ewald@gmail.com>
2023-11-08 13:57:26 -0500

    Updating gems. Removed albacore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'mohawk'
 require_rel 'screens'
 
 include Mohawk::Navigation
-Mohawk.app_path = File.join(File.dirname(__FILE__), '../src/UIA.Extensions.TestApplication/bin/Debug/UIA.Extensions.TestApplication.exe')
+Mohawk.app_path = File.join(File.dirname(__FILE__), '../src/UIA.Extensions.TestApplication/bin/Release/UIA.Extensions.TestApplication.exe')
 
 RSpec.configure do |config|
   config.before(:each) do
